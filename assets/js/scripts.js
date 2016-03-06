8aa5a51470b1be1e681de53529b1effc49036e89
Tori Hedden <tori.hedden@gmail.com>
2016-03-05 20:56:53 -0500

    adds function to populate game board based on 2D array

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,6 +24,7 @@ $(document).ready(function(){
     }
   }//closes board
 
+//unsure if I need this
 var king = "king"
 var queen = "queen"
 var bishop = "bishop"
@@ -49,8 +50,6 @@ var boardPlacer = function(x, y, piece, color){
   piecePlace(arrayConverter(x, y), piece, color);
 }
 
-boardPlacer(3, 1, knight, white);
-
   var board = [
     ['R','N','B','Q','K','B','N','R'],
     ['P','P','P','P','P','P','P','P'],
@@ -61,6 +60,47 @@ boardPlacer(3, 1, knight, white);
     ['p','p','p','p','p','p','p','p'],
     ['r','n','b','q','k','b','n','r'] ];
   //fills the game board with the elements in the array
+
+for (var i = 0 ; i < 8; i++){
+  for (var j = 0; j < 8; j++){
+    var currentPiece = board[i][j];
+    if (currentPiece !== " "){
+      var color
+      if (currentPiece === currentPiece.toUpperCase()) {
+        color = black; //AKA the piece was already upper case
+      } else {
+        color = white;
+      }
+      var pieceID
+      switch(currentPiece.toUpperCase()){
+      case "R":
+          pieceID = rook
+          break;
+      case "N":
+          pieceID = knight
+          break;
+      case "B":
+          pieceID = bishop
+          break;
+      case "Q":
+          pieceID = queen
+          break;
+      case "K":
+          pieceID = king
+          break;
+      case "P":
+          pieceID = pawn
+          break;
+      default:
+          console.log("something's wrong")
+      }
+      boardPlacer(j, i, pieceID, color)
+    }
+  }
+}
+//test for upper/lower case from:
+//http://stackoverflow.com/questions/1027224/how-can-i-test-if-a-letter-in-a-string-is-uppercase-or-lowercase-using-javascrip
+
 
 
 });

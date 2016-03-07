b6812dedd5edba69fa593e776b6532fdcd72e0a3
Tori Hedden <tori.hedden@gmail.com>
2016-03-06 20:09:45 -0500

    adds buttons, pieces iterate step-wise

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
 //generates the game board's styled squares on page load
   for (var i = 0; i < 8; i++){
     if (i % 2 === 0){
-      $("#board-area").append("<div class = 'black square'></div>");
       $("#board-area").append("<div class = 'white square'></div>");
       $("#board-area").append("<div class = 'black square'></div>");
       $("#board-area").append("<div class = 'white square'></div>");
@@ -12,8 +11,8 @@ $(document).ready(function(){
       $("#board-area").append("<div class = 'white square'></div>");
       $("#board-area").append("<div class = 'black square'></div>");
       $("#board-area").append("<div class = 'white square'></div>");
+      $("#board-area").append("<div class = 'black square'></div>");
     } else {
-      $("#board-area").append("<div class = 'white square'></div>");
       $("#board-area").append("<div class = 'black square'></div>");
       $("#board-area").append("<div class = 'white square'></div>");
       $("#board-area").append("<div class = 'black square'></div>");
@@ -21,10 +20,12 @@ $(document).ready(function(){
       $("#board-area").append("<div class = 'black square'></div>");
       $("#board-area").append("<div class = 'white square'></div>");
       $("#board-area").append("<div class = 'black square'></div>");
+      $("#board-area").append("<div class = 'white square'></div>");
     }
   }//closes board
 
-//unsure if I need this
+var board //use this to pass a different array in to the board building functions?
+
 var king = "king"
 var queen = "queen"
 var bishop = "bishop"
@@ -35,22 +36,24 @@ var pawn = "pawn"
 var white = "white"
 var black = "black"
 
-//places piece in again given location
+//places piece in any given location
 var piecePlace = function(index, piece, color){
-  $($(".square")[index]).append("<img src = 'assets/imgs/" + piece + color + ".png'></img>");
+  $($(".square")[index]).html("<img src = 'assets/imgs/" + piece + color + ".png'></img>");
 }
 
 //converts position in 2D array to equivalent position in 1D array
-var arrayConverter = function(x, y){
-  var z =  8 * y + x;
-  return z;
+var arrayConverter = function(x, y, board){
+  var z =  board[y].length * y + x; //each row array is length 8, you could use the constant 8 instead
+  return z; //z is the index of the square in $(".square")
 }
 
-var boardPlacer = function(x, y, piece, color){
-  piecePlace(arrayConverter(x, y), piece, color);
+//combines array converter and piece location functions
+var boardPlacer = function(x, y, piece, color, board){
+  piecePlace(arrayConverter(x, y, board), piece, color);
 }
 
-  var board = [
+//I wanted to do this DRYer, but for now it's wet
+  var board0 = [
     ['R','N','B','Q','K','B','N','R'],
     ['P','P','P','P','P','P','P','P'],
     [' ',' ',' ',' ',' ',' ',' ',' '],
@@ -59,48 +62,173 @@ var boardPlacer = function(x, y, piece, color){
     [' ',' ',' ',' ',' ',' ',' ',' '],
     ['p','p','p','p','p','p','p','p'],
     ['r','n','b','q','k','b','n','r'] ];
-  //fills the game board with the elements in the array
 
-for (var i = 0 ; i < 8; i++){
-  for (var j = 0; j < 8; j++){
-    var currentPiece = board[i][j];
-    if (currentPiece !== " "){
-      var color
-      if (currentPiece === currentPiece.toUpperCase()) {
-        color = black; //AKA the piece was already upper case
+  var board1 = [
+    ['R','N','B','Q','K','B','N','R'],
+    ['P','P','P','P','P','P','P','P'],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ','p',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    ['p','p','p',' ','p','p','p','p'],
+    ['r','n','b','q','k','b','n','r'] ];
+
+  var board2 = [
+    ['R','N','B','Q','K','B',' ','R'],
+    ['P','P','P','P','P','P','P','P'],
+    [' ',' ',' ',' ',' ','N',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ','p',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    ['p','p','p',' ','p','p','p','p'],
+    ['r','n','b','q','k','b','n','r'] ];
+
+  var board3 = [
+    ['R','N','B','Q','K','B',' ','R'],
+    ['P','P','P','P','P','P','P','P'],
+    [' ',' ',' ',' ',' ','N',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ','p','p',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    ['p','p',' ',' ','p','p','p','p'],
+    ['r','n','b','q','k','b','n','r'] ];
+
+  var board4 = [
+    ['R','N','B','Q','K','B',' ','R'],
+    ['P','P','P','P',' ','P','P','P'],
+    [' ',' ',' ',' ','P','N',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ','p','p',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    ['p','p',' ',' ','p','p','p','p'],
+    ['r','n','b','q','k','b','n','r'] ];
+
+var board5 = [
+  ['R','N','B','Q','K','B',' ','R'],
+  ['P','P','P','P',' ','P','P','P'],
+  [' ',' ',' ',' ','P','N',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ','p','p',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ','p',' '],
+  ['p','p',' ',' ','p','p',' ','p'],
+  ['r','n','b','q','k','b','n','r'] ];
+
+var board6 = [
+  ['R','N','B','Q','K','B',' ','R'],
+  ['P','P','P',' ',' ','P','P','P'],
+  [' ',' ',' ',' ','P','N',' ',' '],
+  [' ',' ',' ','P',' ',' ',' ',' '],
+  [' ',' ','p','p',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ','p',' '],
+  ['p','p',' ',' ','p','p',' ','p'],
+  ['r','n','b','q','k','b','n','r'] ];
+
+var board7 =  [
+  ['R','N','B','Q','K','B',' ','R'],
+  ['P','P','P',' ',' ','P','P','P'],
+  [' ',' ',' ',' ','P','N',' ',' '],
+  [' ',' ',' ','P',' ',' ',' ',' '],
+  [' ',' ','p','p',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ','p',' '],
+  ['p','p',' ',' ','p','p','b','p'],
+  ['r','n','b','q','k',' ','n','r'] ];
+
+var board8 = [
+  ['R','N','B','Q','K',' ',' ','R'],
+  ['P','P','P',' ','B','P','P','P'],
+  [' ',' ',' ',' ','P','N',' ',' '],
+  [' ',' ',' ','P',' ',' ',' ',' '],
+  [' ',' ','p','p',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ','p',' '],
+  ['p','p',' ',' ','p','p','b','p'],
+  ['r','n','b','q','k',' ','n','r'] ];
+
+var board9 = [
+  ['R','N','B','Q','K',' ',' ','R'],
+  ['P','P','P',' ','B','P','P','P'],
+  [' ',' ',' ',' ','P','N',' ',' '],
+  [' ',' ',' ','P',' ',' ',' ',' '],
+  [' ',' ','p','p',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ','n','p',' '],
+  ['p','p',' ',' ','p','p','b','p'],
+  ['r','n','b','q','k',' ',' ','r'] ];
+
+var currentBoardIndex = 0
+//the page should load with the initial board setup in place
+
+var boards = [board0, board1, board2, board3, board4, board5, board6, board7, board8, board9]
+
+//fills the game board with the elements in the array
+var drawBoard = function(board){
+  for (var i = 0 ; i < 8; i++){
+    for (var j = 0; j < 8; j++){
+      var currentPiece = board[i][j];
+      if (currentPiece !== " "){
+        var color
+        if (currentPiece === currentPiece.toUpperCase()) {
+          color = black; //AKA the piece was already upper case
+        } else {
+          color = white; //AKA the piece was lower case
+        }
+        var pieceID
+        switch(currentPiece.toUpperCase()){
+        case "R":
+            pieceID = rook
+            break;
+        case "N":
+            pieceID = knight
+            break;
+        case "B":
+            pieceID = bishop
+            break;
+        case "Q":
+            pieceID = queen
+            break;
+        case "K":
+            pieceID = king
+            break;
+        case "P":
+            pieceID = pawn
+            break;
+        default:
+            console.log("something's wrong")
+            //this should never happen if pieces are typed in correctly but just in case
+        }
+        boardPlacer(j, i, pieceID, color, board)
       } else {
-        color = white;
+        $($(".square")[arrayConverter(j, i, board)]).html(" ");
+        //empties the old piece's position
       }
-      var pieceID
-      switch(currentPiece.toUpperCase()){
-      case "R":
-          pieceID = rook
-          break;
-      case "N":
-          pieceID = knight
-          break;
-      case "B":
-          pieceID = bishop
-          break;
-      case "Q":
-          pieceID = queen
-          break;
-      case "K":
-          pieceID = king
-          break;
-      case "P":
-          pieceID = pawn
-          break;
-      default:
-          console.log("something's wrong")
-      }
-      boardPlacer(j, i, pieceID, color)
     }
   }
 }
 //test for upper/lower case from:
 //http://stackoverflow.com/questions/1027224/how-can-i-test-if-a-letter-in-a-string-is-uppercase-or-lowercase-using-javascrip
 
+drawBoard(boards[currentBoardIndex]);
+//the site needs to load with the initial setup in place, AKA board at index boards[0]
+
+  $("#rewind").click(function(){
+    currentBoardIndex = 0
+    drawBoard(boards[currentBoardIndex])
+  });
+
+  //on click of forward, currentBoardIndex ++
+  $("#forward").click(function(){
+    currentBoardIndex++
+    drawBoard(boards[currentBoardIndex])
+  });
+
+  //on click of back, currentBoardIndex --
+  $("#back").click(function(){
+    currentBoardIndex--
+    drawBoard(boards[currentBoardIndex])
+  });
+
+  $("#fastforward").click(function(){
+    currentBoardIndex = (boards.length)-1
+    drawBoard(boards[currentBoardIndex])
+  });
 
 
 });
